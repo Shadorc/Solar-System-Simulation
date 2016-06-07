218c53f8550cd30ca207c51fcb0254b5e50a02f5
Shadorc <bi-crosseur@hotmail.fr>
2016-06-07 13:31:39 +0200

    Add planets

--- Main.py
+++ Main.py
@@ -7,16 +7,27 @@ frame = Frame()
 objects = []
 
 soleil = Planet(None, "soleil.gif", 1.99e30, 1.39e6, 0)
+mercure = Planet(soleil, "mercure.gif", 3.29e23, 4.88e3, 5.79e7)
+venus = Planet(soleil, "venus.gif", 4.87e24, 1.21e4, 1.08e8)
 terre = Planet(soleil, "terre.gif", 5.97e24, 1.27e4, 1.49e8)
 lune = Planet(terre, "lune.gif", 7.35e22, 3.47e3, 3.84e5)
 mars = Planet(soleil, "mars.gif", 6.42e23, 6.78e3, 2.27e8)
-venus = Planet(soleil, "venus.gif", 4.87e24, 1.21e4, 1.08e8)
+jupiter = Planet(soleil, "jupiter.gif", 1.90e27, 1.40e5, 7.79e8) 
+saturne = Planet(soleil, "saturne.gif", 5.68e26, 1.16e5, 1.42e9)
+uranus = Planet(soleil, "uranus.gif", 8.68e25, 5.07e4, 2.88e9)
+neptune = Planet(soleil, "neptune.gif", 1.02e26, 4.92e4, 4.50e9)
+
 
 objects.append(soleil)
+objects.append(mercure)
+objects.append(venus)
 objects.append(terre)
 objects.append(lune)
 objects.append(mars)
-objects.append(venus)
+objects.append(jupiter)
+objects.append(saturne)
+objects.append(uranus)
+objects.append(neptune)
 
 FPS=30
 while True:
