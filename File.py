90e1f369bf325b006f66c0edea7562000460d0de
Shadorc <bi-crosseur@hotmail.fr>
2016-06-08 12:28:49 +0200

    Fix modifs

--- File.py
+++ File.py
@@ -8,23 +8,7 @@ def getInfoPlanet(planetName):
     
     for i in range(len(lines)):
         line = lines[i]
-        mots = line.split(', ')
-        if mots[0] == planetName:
-            return mots
-            
-            
-def getMenu():
-    """Fonction récupérant le menu"""
-    
-    file = open('Menu.txt', 'r')
-    lines = file.readlines()
-    file.close()
-    
-    for i in range(len(lines)):
-        line = lines[i]
-        mots = line.split(', ')
-        if mots[0] == planetName:
-            return mots        infos = line.split(', ')
+        infos = line.split(', ')
         if infos[0] == planetName:
             return infos
     return None
